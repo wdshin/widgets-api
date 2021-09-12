2add47e583d7737ea24691761b46bcf27930749b
Ivan Melnychuk <ivan.melnychuk.me@gmail.com>
2021-09-12 22:08:14 +0300

    Change script player position matching.
    Add league id to script params.
    Use networth as a criteria to sort player positions.

--- src/scripts/parse.matches.js
+++ src/scripts/parse.matches.js
@@ -12,8 +12,12 @@ const opendotaLeagueMatchesURL = (id) => `https://api.opendota.com/api/leagues/$
 const opendotaMatchById = (id) => `https://api.opendota.com/api/matches/${id}`
 
 async function main() {
-    const stats = await getParsedStatsForLeague('13404')
-    console.log(stats)
+    const params = process.argv.slice(2)
+    if (params.length == 1) {
+        const league_id = params.pop()
+        const stats = await getParsedStatsForLeague(league_id)
+        console.log(stats)
+    }
 }
 
 async function getParsedStatsForLeague(league_id) {
@@ -32,7 +36,6 @@ async function getParsedStatsForLeague(league_id) {
     const match_id = league_match_ids[0]
     const match = await get_match_data(match_id)
     const stats = parse_match_stats(match)
-    
     return stats
 }
 
@@ -42,16 +45,22 @@ async function get_match_data(id) {
     return match
 }
 
+function assign_team_and_position(players, isRadiant) {
+    return players.filter((player) => {
+        return player.isRadiant == isRadiant
+    }).sort((left, right) => {
+        return right.net_worth - left.net_worth
+    }).map((player, index) => {
+        player.position = index + 1
+        return player
+    })
+}
+
 function parse_match_stats(match) {
     const players = match.players
-    players.forEach((player) => {
-        const isSupport = is_support(match.players, player)
-        const position = lane_position(player.lane_role, isSupport)
-        player.position = position
-    })
-    
-    const dire_team = players.filter((player) => player.isRadiant == false)
-    const radiant_team = players.filter((player) => player.isRadiant)
+    const radiant_team = assign_team_and_position(players, true)
+    const dire_team = assign_team_and_position(players, false)
+
     const stats = new Map() 
     players.forEach((player) => {
         const player_stats = make_player_stats(
@@ -61,110 +70,111 @@ function parse_match_stats(match) {
         )
         stats.set(player.account_id, player_stats)
     })
+    
     return stats
 }
 
-function vision_score(player) {
-    const obs_score = player.obs_placed + player.observer_uses
-    const sen_score = player.sen_placed + player.sentry_uses
-    const vision_score = obs_score + sen_score
-    return vision_score
-}
-
-function is_support(all, target) {
-    const same_team_players = all.filter((player) => player.isRadiant == target.isRadiant)
-    const same_lane_players = same_team_players.filter((player) => player.lane_role == target.lane_role && player.account_id != target.account_id)
-    if (same_lane_players.length == 1) {
-        const laner = same_lane_players.pop()
-        const is_support = vision_score(target) > vision_score(laner)
-        return is_support
-    } else {
-        return false
+function make_stat_matchup(player, enemy, property) {
+    return {
+        me: player[property],
+        enemy: enemy[property]
     }
 }
 
-function lane_position(lane, isSupport) {
-    if (lane == 2) {
-        return 2
-    } else if (lane == 1) {
-        return isSupport ? 5 : 1
-    } else {
-        return isSupport ? 3 : 4
+function make_stat_matchup(player, enemy, property) {
+    return {
+        me: player[property],
+        enemy: enemy[property]
     }
 }
 
 function make_player_stats(player, ally_team, enemy_team) {
-    const enemy = enemy_team.filter((enemy) => enemy.position == player.position).pop()
+    const enemy = enemy_team[player.position - 1]
     return {
-        win: player.win,
-        lose: player.lose,
-        against: enemy.hero_id,
-        hero: player.hero_id,
-        role: player.lane_role,
-        efficiency: player.lane_efficiency,
-        match_id: player.match_id,
+        player_id: make_stat_matchup(player, enemy, "account_id"),
+        win: make_stat_matchup(player, enemy, "win"),
+        lose: make_stat_matchup(player, enemy, "lose"),
+        hero: make_stat_matchup(player, enemy, "hero_id"),
+        position: make_stat_matchup(player, enemy, "position"),
+        efficiency: make_stat_matchup(player, enemy, "lane_efficiency"),
+        match_id: make_stat_matchup(player, enemy, "match_id"),
         totals: {
-            level: player.level,
-            gold:  player.total_gold,
-            kills: player.kills,
-            deaths: player.deaths,
-            assists: player.assists,
-            kda: player.kda,
-            last_hits: player.last_hits,
-            denies: player.denies
+            level: make_stat_matchup(player, enemy, "level"),
+            gold: make_stat_matchup(player, enemy, "total_gold"),
+            net_worth: make_stat_matchup(player, enemy, "net_worth"),
+            kills: make_stat_matchup(player, enemy, "kills"),
+            deaths: make_stat_matchup(player, enemy, "deaths"),
+            assists: make_stat_matchup(player, enemy, "assists"),
+            kda: make_stat_matchup(player, enemy, "kda"),
+            last_hits: make_stat_matchup(player, enemy, "last_hits"),
+            denies: make_stat_matchup(player, enemy, "denies")
         },
-        items: [
-            player.item_0,
-            player.item_1,
-            player.item_2,
-            player.item_3,
-            player.item_4,
-            player.item_5
-        ],
-        neutral_item: player.item_neutral,
+        items: {
+            me: [player.item_0, player.item_1, player.item_2, player.item_3, player.item_4, player.item_5],
+            enemy: [enemy.item_0, enemy.item_1, enemy.item_2, enemy.item_3, enemy.item_4, enemy.item_5]
+        },
+        neutral_item: make_stat_matchup(player, enemy, "item_neutral"),
         per_minute: {
-            actions:  player.actions_per_min,
-            gold: player.gold_per_min,
-            xp: player.xp_per_min,
-            kills: player.kills_per_min
+            actions:  make_stat_matchup(player, enemy, "actions_per_min"),
+            gold: make_stat_matchup(player, enemy, "gold_per_min"),
+            xp: make_stat_matchup(player, enemy, "xp_per_min"),
+            kills: make_stat_matchup(player, enemy, "kills_per_min")
         },
         camps: {
-            stacked: player.camps_stacked,
-            creeps: player.creeps_stacked,
-            neutrals_killed: player.neutral_kills,
-            ancient_kills: player.ancient_kills
+            stacked: make_stat_matchup(player, enemy, "camps_stacked"),
+            creeps: make_stat_matchup(player, enemy, "creeps_stacked"),
+            neutrals_killed: make_stat_matchup(player, enemy, "neutral_kills"),
+            ancient_kills: make_stat_matchup(player, enemy, "ancient_kills")
         },
         wards: {
             observer: {
-                placed: player.obs_placed,
-                used: player.observer_uses,
-                killed: player.observer_kills
+                placed: make_stat_matchup(player, enemy, "obs_placed"),
+                used: make_stat_matchup(player, enemy, "observer_uses"),
+                killed: make_stat_matchup(player, enemy, "observer_kills")
             },
             sentry: {
-                placed: player.sen_placed,
-                used: player.sentry_uses,
-                killed: player.sentry_kills
+                placed: make_stat_matchup(player, enemy, "sen_placed"),
+                used: make_stat_matchup(player, enemy, "sentry_uses"),
+                killed: make_stat_matchup(player, enemy, "sentry_kills")
             }
         },
         creeps: {
-            killed: player.lane_kills,
-            denied: player.denies
+            killed: make_stat_matchup(player, enemy, "lane_kills"),
+            denied: make_stat_matchup(player, enemy, "denies")
         },
         tower: {
-            damage: player.tower_damage,
-            killed: player.towers_killed,
-            kills: player.tower_kills
+            damage: make_stat_matchup(player, enemy, "tower_damage"),
+            killed: make_stat_matchup(player, enemy, "towers_killed"),
+            kills: make_stat_matchup(player, enemy, "tower_kills")
         }
     }
 }
 
 main()
 
-
 /*
-- Add league id to script params
 - Add wait before sending the match details request
 - Sum up stats
 - Saving stats to db
 - Adding facts
+*/
+
+/*
+    Player on Hero vs Enemy Hero in match
+    Player on Hero vs Enemy Hero in tournament
+    Player on Hero vs avg on this Position in tournament
+    Player on Hero vs avg on this Hero in tournament
+    Player on Hero vs avg on this Hero for this player in tournament
+    Player on Hero vs Enemy (any Hero) player in torunament
+*/
+
+/*
+    We get league id.
+    Find all matches for this league.
+    For each match we distribute player positions according to their networth.
+    Using player positions we match the stats of two oposing players to create a matchup.
+    Every matchup will be recording the stats for
+    - the Player
+    - the Hero
+    Having matchups accumulated we can easily pick and compare any of them for both hero & player
 */
